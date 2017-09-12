e13de76b2e111151154db16566b7c367711a7d21
Emanuel Oliveira <emanuel.oliveira23@gmail.com>
2017-09-11 22:54:08 -0300

    List 1, exxercise 3 completed. List done

--- list1/ex3.m
+++ list1/ex3.m
@@ -17,5 +17,50 @@ test_y = x(training_size+1:training_size+test_size, :);
 
 
 lambdas = [0; 1; 2; 3; 4; 5];
-res = regular_batch(training_x, training_y, lambdas);
-disp(res);
+lambda_weights = regular_batch(training_x, training_y, lambdas);
+
+% Item 1: print weights by lambda
+disp(lambda_weights);
+
+% Item 2: lambda x Square Mean Erro in training and test sets
+
+% Util function
+function errors = sme(x, y, weights)
+  x_size = size(x, 1);
+  weights_size = size(weights, 1);
+
+  errors = zeros(weights_size, 1);
+  for l = 1:weights_size
+    
+    error = 0;
+    w = weights(l);
+    
+    for i = 1:x_size
+      xi = x(i);
+      yi = y(i);
+      y_ = xi * w;
+      e_ = y_ - yi;
+      error = error + e_ * e_;
+    endfor
+    
+    errors(l,:) = error / x_size;
+    
+  endfor 
+endfunction
+
+% Item 2.1: lambda x Square Mean Erro in training
+training_errors = sme(training_x, training_y, lambda_weights);
+figure(1);
+plot(lambdas, training_errors);
+
+
+% Item 2.2: lambda x Square Mean Erro in training
+test_errors = sme(test_x, training_y, lambda_weights); 
+figure(2);
+plot(lambdas, test_errors);
+
+% Item extra: plotting the 2 graphs in same figure
+figure(3);
+plot(lambdas, training_errors, 'g');
+hold on;
+plot(lambdas, test_errors, 'r');
