64fb08017ec7479a61339732836d73592ad38e24
Emanuel Oliveira <emanuel.oliveira23@gmail.com>
2017-09-13 11:52:24 -0300

    refactoring

--- list1/regular_batch.m
+++ list1/regular_batch.m
@@ -1,23 +1,19 @@
 function res = regular_batch(x, y, lambdas)
   
+  rows = size(x, 1); % sample
   cols = size(x, 2); % features
-  rows = size(x, 1);
-  
-  _x = [ones(rows, 1) x];
-  _cols = size(_x, 2);
-  
-  I = eye(_cols);
-  
-  % Não utilizar regularização no termo w0
-  I(1,1) = 0;
-  
   lambdas_size = size(lambdas, 1);
   
-  res = zeros(lambdas_size, _cols); % res = weights
+  _x = [ones(rows, 1) x];
+  cols_size = size(_x, 2);
+  I = eye(cols_size);
   
+  res = zeros(lambdas_size, cols_size); % res = weights
   for l = 1:lambdas_size
     lambda = lambdas(l, 1);
-    res(l, :) = inv(_x' * _x + lambda * I) * _x' * y;
+    lambdaI = lambda * I;
+    lambdaI(1,1) = 0;  % "Não utilizar regularização no termo w0"
+    res(l, :) = inv(_x' * _x + lambdaI) * _x' * y;
   endfor
   
 endfunction
